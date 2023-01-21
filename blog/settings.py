16f9f49e7003101f03bc13dcfb8be3b00c13ed19
modd <smarthackershanib@gmail.com>
2023-01-20 22:24:58 -0800

    remove 500 while debug false

--- blog/settings.py
+++ blog/settings.py
@@ -133,4 +133,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
